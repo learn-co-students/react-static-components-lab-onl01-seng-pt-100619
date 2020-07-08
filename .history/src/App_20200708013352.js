c962c7a646c80de893f878e437a9b6e33d0a10af
steelerx155 <steelerx155@gmail.com>
2020-07-08 01:52:00 -0400

    Done.

---
+++ .history/src/App_20200708013352.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import CatComponent,{Component} from 'react'
+
+class App extends Component {
+	render() {
+		// your code in the return statement below!
+		return (
+			<div className="App">
+				<CatComponent />
+				<GraceHopperQuoteComponent />
+				{/* one more component missing */}
+			</div>
+		);
+	}
+}
+
+export default App;
